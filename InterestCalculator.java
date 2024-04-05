c030aaa066e71cbe1dd5e6fc826b532fa69bb52b
Gowtham <gowtham@default.com>
2024-04-05 22:51:19 +0530

    InterestCalculator

---
+++ InterestCalculator.java
@@ -0,0 +1,9 @@
+public class InterestCalculator {
+
+    public InterestCalculator() {
+    }
+    public static void calculate(){
+        System.out.println("Welcome to the Interest Calculator");
+    }
+
+}
